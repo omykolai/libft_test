d7e565553e4338c14bb1fb0b367c6d7d44d11381
Oleksandr MYKOLAICHUK <omykolai@e3r1p7.unit.ua>
2018-01-29 13:15:15 +0200

    norm check added

--- test_lib.sh
+++ test_lib.sh
@@ -1,9 +1,22 @@
+
 GREEN='\033[0;32m'
+RED='\033[0;31m'
 NORMAL='\033[0m'
+
+mv test_lib.c test_lib
+
+echo
+NORM=$(norminette | grep -e Error)
+if [ "$NORM" != "" ]
+then
+	echo "${RED}NORM ERROR"
+	echo
+fi
+
+mv test_lib test_lib.c
 
 make > make.log
 gcc -Wall -Wextra -Werror -o test_lib test_lib.c -L. -lft
-echo
 ./test_lib 0 | grep ">>"
 ./test_lib 1 | grep ">>"
 ./test_lib 2 | grep ">>"
@@ -12,8 +25,15 @@ echo
 ./test_lib 5 | grep ">>"
 ./test_lib 6 | grep ">>"
 test -f res
-[ -f res ] && chmod 666 res && rm res || echo "${GREEN}All tests passed!${NORMAL}"
-echo
+if [ -f res ]
+then
+	chmod 666 res && rm res
+	echo
+elif [ "$NORM" == "" ]
+then
+	echo "${GREEN}All tests passed!${NORMAL}"
+	echo
+fi
 chmod 777 test_lib
 rm test_lib
 make fclean > make.log
